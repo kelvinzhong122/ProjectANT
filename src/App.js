f260e5f2bd099efe3a83907dd976763138b32f7d
Kelvin Zhong <KelvinZ@mossadams.ma>
2021-07-21 23:13:29 -0700

    tester

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
-import logo from './logo.svg';
-import profileimage from './Img/MCLAREN 1.jpg'
+import React from 'react';
+import Navbar from './components/Navbar';
 import './App.css';
+import Home from './components/pages/Home';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import SignUp from './components/pages/SignUp';
+import Project1 from './components/pages/Project1'
 
 function App() {
   return (
-    <div className="App">
-      <h1>ah ha ha</h1>
-      <img src = {profileimage} alt ="profit image"/>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/services' component={Services} />
+          <Route path='/products' component={Products} />
+          <Route path='/sign-up' component={SignUp} />
+          <Route path='/Project1' component={Project1}/>
+        </Switch>
+      </Router>
+    </>
   );
 }
 
