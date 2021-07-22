fc4b6b58615ecea3f2774b69dc63a002d86ab111
Kelvin Zhong <KelvinZ@mossadams.ma>
2021-07-21 17:55:54 -0700

    Updates

---
+++ precache-manifest.a6332f3b42263df219ec31df40be46c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b18ee0341100a0ef1a50cb3e20fd7f4",
+    "url": "/ProjectFail/index.html"
+  },
+  {
+    "revision": "0eacd507c6abd7a93d0d",
+    "url": "/ProjectFail/static/css/main.74468b2e.chunk.css"
+  },
+  {
+    "revision": "95b4d5db43dafec2f190",
+    "url": "/ProjectFail/static/js/2.469116f9.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/ProjectFail/static/js/2.469116f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0eacd507c6abd7a93d0d",
+    "url": "/ProjectFail/static/js/main.085fb2e5.chunk.js"
+  },
+  {
+    "revision": "051fe6613a82f67b57c6",
+    "url": "/ProjectFail/static/js/runtime-main.37924d6e.js"
+  }
+]);
